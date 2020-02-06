052bbcf2f1b34a13a2977cde89cb899e9dc9ceaa
Marco Hernandez <marco.hernandez@jalasoft.com>
2020-02-06 10:01:36 -0500

    removed unused counters in sources

--- main.rb
+++ main.rb
@@ -4,16 +4,12 @@ sources = Array [
   {
     "uri" => "https://www.lostiempos.com",
     "strategy" => 'LosTiemposScrapeStrategy',
-    "selector" => '.views-row, table .col-1, table .col-2, table .col-3, table .col-4',
-    "records" => 0,
-    "duration" => 0
+    "selector" => '.views-row, table .col-1, table .col-2, table .col-3, table .col-4'
   },
   {
     "uri" => "https://www.opinion.com.bo",
     "strategy" => 'OpinionBoliviaScrapeStrategy',
-    "selector" => 'article, .more-news-section-links li',
-    "records" => 0,
-    "duration" => 0
+    "selector" => 'article, .more-news-section-links li'
   },
 ]
 
